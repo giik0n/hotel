6fdcc31650ba134d1fc4c31cbbb3adb6e0dc93ca
giik0n <geek4true@gmail.com>
2020-08-19 20:29:21 +0200

    CrossOrigin

--- src/main/java/pan/edu/hotel/controller/rest/ClientRestController.java
+++ src/main/java/pan/edu/hotel/controller/rest/ClientRestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RequestMapping("/api/client/v2")
 @RestController
+@CrossOrigin
 public class ClientRestController {
     @Autowired
     ClientServiceImpl clientService;
